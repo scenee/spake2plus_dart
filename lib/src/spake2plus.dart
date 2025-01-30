194ce3bb0d6d9ac72bbea9a67837dcad0abe6998
Shin Yamamoto <shin@scenee.com>
2025-01-30 18:16:32 +0900

    Fix memory leaks

--- lib/src/spake2plus.dart
+++ lib/src/spake2plus.dart
@@ -17,29 +17,44 @@ class Spake2plus {
   final Uint8List _curvePointM; // a big-endian bytes of M point
   final Uint8List _curvePointN; // a big-endian bytes of N point
 
-  late final ffi.Pointer<BN_CTX> _ctx;
-  late final ffi.Pointer<EC_GROUP> _group;
-  late final ffi.Pointer<BIGNUM> _order;
-
   Spake2plus(String libraryPath, List<int> curvePointM, List<int> curvePointN)
       : _openssl = OpenSSL(ffi.DynamicLibrary.open(libraryPath)),
         _curvePointM = Uint8List.fromList(curvePointM),
-        _curvePointN = Uint8List.fromList(curvePointN) {
-    _ctx = _openssl.BN_CTX_new();
-    _group = _openssl.EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1);
-    _order = _openssl.BN_new();
-    final res = _openssl.EC_GROUP_get_order(_group, _order, _ctx);
-    assert(res == 1);
+        _curvePointN = Uint8List.fromList(curvePointN);
+
+  T _context<T>(
+      T Function(
+        ffi.Pointer<EC_GROUP>,
+        ffi.Pointer<BN_CTX>,
+        ffi.Pointer<BIGNUM>,
+      ) fn) {
+    final group = _openssl.EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1);
+    final ctx = _openssl.BN_CTX_new();
+    final order = _openssl.BN_new();
+    final res = _openssl.EC_GROUP_get_order(group, order, ctx);
+    if (res != 1) {
+      throw Exception("Failed to get order of the curve");
+    }
+    try {
+      return fn(group, ctx, order);
+    } finally {
+      _openssl.BN_CTX_free(ctx);
+      _openssl.EC_GROUP_free(group);
+      _openssl.BN_free(order);
+    }
   }
 
   (Uint8List, Uint8List) computeWitnesses(ScryptParameters scryptParams) {
-    return _computeWitnesses(scryptParams, _group, _ctx);
+    return _context((group, ctx, order) {
+      return _computeWitnesses(scryptParams, group, ctx, order);
+    });
   }
 
   (Uint8List, Uint8List) _computeWitnesses(
     ScryptParameters scryptParams,
     ffi.Pointer<EC_GROUP> group,
     ffi.Pointer<BN_CTX> ctx,
+    ffi.Pointer<BIGNUM> order,
   ) {
     final w0 = _openssl.BN_new();
     final w1 = _openssl.BN_new();
@@ -55,13 +70,13 @@ class Spake2plus {
     final z0 = _openssl.BN_bin2bn(dk, mid, ffi.nullptr);
     final z1 = _openssl.BN_bin2bn(dk + mid, mid, ffi.nullptr);
 
-    _openssl.calculateW(w0, z0, _order, ctx);
-    _openssl.calculateW(w1, z1, _order, ctx);
+    _openssl.calculateW(w0, z0, order, ctx);
+    _openssl.calculateW(w1, z1, order, ctx);
 
     assert(_openssl.BN_cmp(w0, w1) == 1);
 
-    final w0Bytes = _openssl.bn2bytes(_group, w0, 32, _ctx);
-    final w1Bytes = _openssl.bn2bytes(_group, w1, 32, _ctx);
+    final w0Bytes = _openssl.bn2bytes(group, w0, 32, ctx);
+    final w1Bytes = _openssl.bn2bytes(group, w1, 32, ctx);
 
     _openssl.BN_free(z1);
     _openssl.BN_free(z0);
@@ -82,169 +97,170 @@ class Spake2plus {
   }
 
   Uint8List randomValidScalar() {
-    final p = _openssl.BN_new();
-    _openssl.BN_rand_range(p, _order);
-    final ret = _openssl.bn2bytes(_group, p, 32, _ctx);
-    _openssl.BN_free(p);
-    return ret;
+    return _context((group, ctx, order) {
+      final p = _openssl.BN_new();
+      _openssl.BN_rand_range(p, order);
+      final ret = _openssl.bn2bytes(group, p, 32, ctx);
+      _openssl.BN_free(p);
+      return ret;
+    });
   }
 
   // shareP = X
   Uint8List computeShareP(Uint8List w0bytes, Uint8List xBytes) {
-    final group = _group;
-    final ctx = _ctx;
+    return _context((group, ctx, order) {
+      final w0 = _openssl.BN_new();
+      _openssl.bytes2bn(w0, w0bytes);
 
-    final w0 = _openssl.BN_new();
-    _openssl.bytes2bn(w0, w0bytes);
+      final M = _openssl.EC_POINT_new(group);
+      _openssl.bytes2point(group, M, _curvePointM, ctx);
 
-    final M = _openssl.EC_POINT_new(group);
-    _openssl.bytes2point(group, M, _curvePointM, ctx);
+      final x = _openssl.BN_new();
+      _openssl.bytes2bn(x, xBytes);
 
-    final x = _openssl.BN_new();
-    _openssl.bytes2bn(x, xBytes);
+      final X = _openssl.calculatePoint(group, ctx, x, M, w0);
 
-    final X = _openssl.calculatePoint(group, ctx, x, M, w0);
+      final bytesX = _openssl.point2bytes(group, X, ctx);
 
-    final bytesX = _openssl.point2bytes(group, X, ctx);
+      _openssl.EC_POINT_free(X);
+      _openssl.BN_free(x);
+      _openssl.EC_POINT_free(M);
+      _openssl.BN_free(w0);
 
-    _openssl.EC_POINT_free(X);
-    _openssl.BN_free(x);
-    _openssl.EC_POINT_free(M);
-    _openssl.BN_free(w0);
-
-    return bytesX;
+      return bytesX;
+    });
   }
 
   // shareV = Y
   Uint8List computeShareV(Uint8List w0Bytes, Uint8List yBytes) {
-    final group = _group;
-    final ctx = _ctx;
+    return _context((group, ctx, _) {
+      final w0 = _openssl.BN_new();
+      _openssl.bytes2bn(w0, w0Bytes);
 
-    final w0 = _openssl.BN_new();
-    _openssl.bytes2bn(w0, w0Bytes);
+      final N = _openssl.EC_POINT_new(group);
+      _openssl.bytes2point(group, N, _curvePointN, ctx);
 
-    final N = _openssl.EC_POINT_new(group);
-    _openssl.bytes2point(group, N, _curvePointN, ctx);
+      final y = _openssl.BN_new();
+      _openssl.bytes2bn(y, yBytes);
 
-    final y = _openssl.BN_new();
-    _openssl.bytes2bn(y, yBytes);
+      final Y = _openssl.calculatePoint(group, ctx, y, N, w0);
 
-    final Y = _openssl.calculatePoint(group, ctx, y, N, w0);
+      final bytesY = _openssl.point2bytes(group, Y, ctx);
 
-    final bytesY = _openssl.point2bytes(_group, Y, _ctx);
+      _openssl.BN_free(y);
+      _openssl.BN_free(w0);
+      _openssl.EC_POINT_free(Y);
+      _openssl.EC_POINT_free(N);
 
-    _openssl.BN_free(y);
-    _openssl.BN_free(w0);
-    _openssl.EC_POINT_free(Y);
-    _openssl.EC_POINT_free(N);
-
-    return bytesY;
+      return bytesY;
+    });
   }
 
   Uint8List computeRecordL(Uint8List w1Bytes) {
-    final group = _group;
-    final ctx = _ctx;
+    return _context((group, ctx, _) {
+      final w1 = _openssl.BN_new();
+      final L = _openssl.EC_POINT_new(group);
 
-    final w1 = _openssl.BN_new();
-    final L = _openssl.EC_POINT_new(group);
+      _openssl.bytes2bn(w1, w1Bytes);
+      _openssl.EC_POINT_mul(
+          group, L, w1, ffi.nullptr, ffi.nullptr, ctx); // w1 * G
 
-    _openssl.bytes2bn(w1, w1Bytes);
-    _openssl.EC_POINT_mul(
-        group, L, w1, ffi.nullptr, ffi.nullptr, ctx); // w1 * G
+      final bytesL = _openssl.point2bytes(group, L, ctx);
 
-    final bytesL = _openssl.point2bytes(_group, L, _ctx);
+      _openssl.BN_free(w1);
+      _openssl.EC_POINT_free(L);
 
-    _openssl.BN_free(w1);
-    _openssl.EC_POINT_free(L);
-
-    return bytesL;
+      return bytesL;
+    });
   }
 
   (Uint8List Z, Uint8List V)? proverFinish(Uint8List w0Bytes, Uint8List w1Bytes,
       Uint8List xBytes, Uint8List bytesY) {
-    final group = _group;
-    final ctx = _ctx;
+    return _context((group, ctx, _) {
+      final Y = _openssl.EC_POINT_new(group);
+      _openssl.bytes2point(group, Y, bytesY, ctx);
+      final ok = (_openssl.EC_POINT_is_on_curve(group, Y, ctx) == 1) &&
+          (_openssl.EC_POINT_is_at_infinity(group, Y) == 0);
 
-    final Y = _openssl.EC_POINT_new(group);
-    _openssl.bytes2point(group, Y, bytesY, ctx);
-    final ok = (_openssl.EC_POINT_is_on_curve(group, Y, ctx) == 1) &&
-        (_openssl.EC_POINT_is_at_infinity(group, Y) == 0);
+      if (!ok) {
+        _openssl.EC_POINT_free(Y);
+        return null;
+      }
 
-    if (!ok) {
+      final N = _openssl.EC_POINT_new(group);
+      _openssl.bytes2point(group, N, _curvePointN, ctx);
+
+      final x = _openssl.BN_new();
+      _openssl.bytes2bn(x, xBytes);
+
+      final w0 = _openssl.BN_new();
+      _openssl.bytes2bn(w0, w0Bytes);
+
+      final w1 = _openssl.BN_new();
+      _openssl.bytes2bn(w1, w1Bytes);
+
+      final Z = _openssl.calculateZ(group, ctx, Y, N, w0, x);
+      final V = _openssl.calculateV_prover(group, ctx, Y, N, w0, w1, x);
+
+      final bytesZ = _openssl.point2bytes(group, Z, ctx);
+      final bytesV = _openssl.point2bytes(group, V, ctx);
+
+      _openssl.EC_POINT_free(V);
+      _openssl.EC_POINT_free(Z);
+      _openssl.EC_POINT_free(N);
       _openssl.EC_POINT_free(Y);
-      return null;
-    }
+      _openssl.BN_free(w1);
+      _openssl.BN_free(w0);
+      _openssl.BN_free(x);
 
-    final N = _openssl.EC_POINT_new(group);
-    _openssl.bytes2point(group, N, _curvePointN, ctx);
-
-    final x = _openssl.BN_new();
-    _openssl.bytes2bn(x, xBytes);
-
-    final w0 = _openssl.BN_new();
-    _openssl.bytes2bn(w0, w0Bytes);
-
-    final w1 = _openssl.BN_new();
-    _openssl.bytes2bn(w1, w1Bytes);
-
-    final Z = _openssl.calculateZ(group, ctx, Y, N, w0, x);
-    final V = _openssl.calculateV_prover(group, ctx, Y, N, w0, w1, x);
-
-    final bytesZ = _openssl.point2bytes(group, Z, ctx);
-    final bytesV = _openssl.point2bytes(group, V, ctx);
-
-    _openssl.EC_POINT_free(V);
-    _openssl.EC_POINT_free(Z);
-    _openssl.EC_POINT_free(N);
-    _openssl.EC_POINT_free(Y);
-    _openssl.BN_free(w1);
-    _openssl.BN_free(w0);
-    _openssl.BN_free(x);
-
-    return (bytesZ, bytesV);
+      return (bytesZ, bytesV);
+    });
   }
 
   (Uint8List Z, Uint8List V)? verifierFinish(
-      Uint8List w0Bytes, Uint8List bytesL, Uint8List bytesX, Uint8List yBytes) {
-    final group = _group;
-    final ctx = _ctx;
+    Uint8List w0Bytes,
+    Uint8List bytesL,
+    Uint8List bytesX,
+    Uint8List yBytes,
+  ) {
+    return _context((group, ctx, _) {
+      final X = _openssl.EC_POINT_new(group);
+      _openssl.bytes2point(group, X, bytesX, ctx);
+      final ok = (_openssl.EC_POINT_is_on_curve(group, X, ctx) == 1) &&
+          (_openssl.EC_POINT_is_at_infinity(group, X) == 0);
 
-    final X = _openssl.EC_POINT_new(group);
-    _openssl.bytes2point(group, X, bytesX, ctx);
-    final ok = (_openssl.EC_POINT_is_on_curve(group, X, ctx) == 1) &&
-        (_openssl.EC_POINT_is_at_infinity(group, X) == 0);
+      if (!ok) {
+        _openssl.EC_POINT_free(X);
+        return null;
+      }
 
-    if (!ok) {
+      final M = _openssl.EC_POINT_new(group);
+      _openssl.bytes2point(group, M, _curvePointM, ctx);
+
+      final L = _openssl.EC_POINT_new(group);
+      _openssl.bytes2point(group, L, bytesL, ctx);
+
+      final w0 = _openssl.BN_new();
+      _openssl.bytes2bn(w0, w0Bytes);
+      final y = _openssl.BN_new();
+      _openssl.bytes2bn(y, yBytes);
+
+      final Z = _openssl.calculateZ(group, ctx, X, M, w0, y);
+      final V = _openssl.calculateV_verifier(group, ctx, L, y);
+
+      final bytesZ = _openssl.point2bytes(group, Z, ctx);
+      final bytesV = _openssl.point2bytes(group, V, ctx);
+
+      _openssl.EC_POINT_free(V);
+      _openssl.EC_POINT_free(Z);
+      _openssl.EC_POINT_free(L);
+      _openssl.EC_POINT_free(M);
       _openssl.EC_POINT_free(X);
-      return null;
-    }
+      _openssl.BN_free(w0);
+      _openssl.BN_free(y);
 
-    final M = _openssl.EC_POINT_new(group);
-    _openssl.bytes2point(group, M, _curvePointM, ctx);
-
-    final L = _openssl.EC_POINT_new(group);
-    _openssl.bytes2point(group, L, bytesL, ctx);
-
-    final w0 = _openssl.BN_new();
-    _openssl.bytes2bn(w0, w0Bytes);
-    final y = _openssl.BN_new();
-    _openssl.bytes2bn(y, yBytes);
-
-    final Z = _openssl.calculateZ(group, ctx, X, M, w0, y);
-    final V = _openssl.calculateV_verifier(group, ctx, L, y);
-
-    final bytesZ = _openssl.point2bytes(group, Z, ctx);
-    final bytesV = _openssl.point2bytes(group, V, ctx);
-
-    _openssl.EC_POINT_free(V);
-    _openssl.EC_POINT_free(Z);
-    _openssl.EC_POINT_free(L);
-    _openssl.EC_POINT_free(M);
-    _openssl.EC_POINT_free(X);
-    _openssl.BN_free(w0);
-    _openssl.BN_free(y);
-
-    return (bytesZ, bytesV);
+      return (bytesZ, bytesV);
+    });
   }
 
   Uint8List computeTranscript(
@@ -298,14 +314,17 @@ class Spake2plus {
       data[i] = input[i];
     }
     final ffi.Pointer<EVP_MD> type;
+    final int buflen;
     if (sn == SN_sha256) {
       type = _openssl.EVP_sha256();
+      buflen = 32;
     } else if (sn == SN_sha512) {
       type = _openssl.EVP_sha512();
+      buflen = 64;
     } else {
       throw ArgumentError("Unsupported digest algorithm: $sn");
     }
-    final hash = calloc<ffi.UnsignedChar>(256);
+    final hash = calloc<ffi.UnsignedChar>(buflen);
     final mdlen = calloc<ffi.UnsignedInt>(1);
     _openssl.EVP_Digest(
       data.cast(),
@@ -400,18 +419,26 @@ class Spake2plus {
   }
 
   Uint8List convertToUncompressedECPoint(Uint8List pointData) {
-    final cp = _openssl.EC_POINT_new(_group);
-    final buf = pointData.toPointer();
-    _openssl.EC_POINT_oct2point(_group, cp, buf, pointData.length, _ctx);
+    return _context((group, ctx, _) {
+      final cp = _openssl.EC_POINT_new(group);
+      final buf = pointData.toPointer();
+      _openssl.EC_POINT_oct2point(group, cp, buf, pointData.length, ctx);
 
-    final len = 65;
-    final ptr = calloc<ffi.UnsignedChar>(len);
-    _openssl.EC_POINT_point2oct(_group, cp,
-        point_conversion_form_t.POINT_CONVERSION_UNCOMPRESSED, ptr, len, _ctx);
-    final ret = Uint8List.fromList(ptr.cast<ffi.Uint8>().asTypedList(len));
-    _openssl.EC_POINT_free(cp);
-    calloc.free(buf);
-    calloc.free(ptr);
-    return ret;
+      final len = 65;
+      final ptr = calloc<ffi.UnsignedChar>(len);
+      _openssl.EC_POINT_point2oct(
+        group,
+        cp,
+        point_conversion_form_t.POINT_CONVERSION_UNCOMPRESSED,
+        ptr,
+        len,
+        ctx,
+      );
+      final ret = Uint8List.fromList(ptr.cast<ffi.Uint8>().asTypedList(len));
+      _openssl.EC_POINT_free(cp);
+      calloc.free(buf);
+      calloc.free(ptr);
+      return ret;
+    });
   }
 }
